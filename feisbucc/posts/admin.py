fc8bae08890d4b57bc3b482deafcba1a74091183
lollobelloso <87982272+lollobelloso@users.noreply.github.com>
2024-05-31 17:00:35 +0200

    Delete feisbucc directory

--- feisbucc/posts/admin.py
+++
@@ -1,6 +0,0 @@
-# posts/admin.py
-
-from django.contrib import admin
-from .models import Post
-
-admin.site.register(Post)
--- feisbucc/settings/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import UserSettings
-
-class UserSettingsForm(forms.ModelForm):
-    class Meta:
-        model = UserSettings
-        fields = ['receive_newsletters', 'public_profile']
